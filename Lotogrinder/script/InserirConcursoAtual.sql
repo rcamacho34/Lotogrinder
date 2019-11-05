41d96efab99fc3e48e1b2f5061115c378ca519c7
Ronaldo Camacho <ronaldocamacho@gmail.com>
2019-11-04 23:32:25 -0300

    scrpis

--- Lotogrinder/script/InserirConcursoAtual.sql
+++ Lotogrinder/script/InserirConcursoAtual.sql
@@ -1,11 +1,7 @@
 
-INSERT INTO tbConcurso (Id, DataSorteio, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15)
-VALUES (1877, convert(date, '2019-10-14'), 2,4,8,9,11,12,13,14,15,18,19,21,23,24,25)
-
 
 INSERT INTO tbConcurso (Id, DataSorteio, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15)
-VALUES (1878, convert(date, '2019-10-16'), 1,2,3,4,5,6,8,10,12,15,17,19,23,24,25)
-
+VALUES (1884, convert(date, '2019-10-30'), 1,2,3,5,6,7,8,9,11,12,14,16,17,23,25)
 
 
 
